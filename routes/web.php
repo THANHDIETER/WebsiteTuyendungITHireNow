1e202874fca0ea2bd1ca26d09bea7ab9a51a3fdf
SonwTungf <sonwtungf.2004@gmail.com>
2025-05-25 20:14:46 +0700

    note + dinh nghia

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm']);
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegisterForm']);
+Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
