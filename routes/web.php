f1dde95d172fb060ae224034321be21a9187074c
SonwTungf <sonwtungf.2004@gmail.com>
2025-05-25 21:14:10 +0700

    route map

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+require __DIR__.'/admin.php';
+require __DIR__.'/employer.php';
+require __DIR__.'/jobseeker.php';
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,3 +15,7 @@ Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLog
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
 Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegisterForm']);
 Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+Route::get('/docs', function () {
+    return view('docs.index');
+});
