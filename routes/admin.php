572a4100bc052f8bade5a8b390de57140ee1c467
tuntph52384 <tuntph52384@gmail.com>
2025-05-28 20:39:15 +0700

    Job Post Approval Feature

--- routes/admin.php
+++ routes/admin.php
@@ -3,4 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 
-Route::get('/admin/users', [UserController::class, 'index']);
+use App\Http\Controllers\Admin\JobController;
+
+Route::get('/admin', [UserController::class, 'index']);
+
+
+
+// Route::prefix('admin')->middleware('admin')->group(function () {
+Route::prefix('admin')->group(function () {
+    Route::get('/jobs', [JobController::class, 'index'])->name('admin.jobs.index');
+    Route::patch('/jobs/{id}/approve', [JobController::class, 'approve'])->name('admin.jobs.approve');
+    Route::delete('/jobs/{id}', [JobController::class, 'destroy'])->name('admin.jobs.destroy');
+});
