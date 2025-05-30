2c2f10927cb484815a07c0d5835507c02dc862a0
sonbtph52402 <sonbtph52402@gmail.com>
2025-05-30 20:24:17 +0700

    check

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\AdminResumeController;
 
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Admin\JobApiController;
+
+////
+use App\Http\Middleware\AuthMiddleware;
+use App\Http\Middleware\RoleMiddleware;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -36,4 +40,19 @@ Route::prefix('admin')->middleware(['auth:api', 'admin'])->group(function () {
     Route::get('/jobs', [JobApiController::class, 'index'])->name('api.admin.jobs.index');
     Route::patch('/jobs/{id}/approve', [JobApiController::class, 'approve'])->name('api.admin.jobs.approve');
     Route::delete('/jobs/{id}', [JobApiController::class, 'destroy'])->name('api.admin.jobs.destroy');
+});
+
+
+/////////
+Route::prefix('admin')->middleware([AuthMiddleware::class])->group(function () {
+    Route::get('/resumes', [AdminResumeController::class, 'index']);
+    Route::patch('/resumes/{id}/approve', [AdminResumeController::class, 'approve']);
+    Route::delete('/resumes/{id}/delete', [AdminResumeController::class, 'destroy']);
+
+    // Dùng alias 'role:admin' thay cho closure
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/jobs', [JobApiController::class, 'index'])->name('api.admin.jobs.index');
+        Route::patch('/jobs/{id}/approve', [JobApiController::class, 'approve'])->name('api.admin.jobs.approve');
+        Route::delete('/jobs/{id}', [JobApiController::class, 'destroy'])->name('api.admin.jobs.destroy');
+    });
 });
