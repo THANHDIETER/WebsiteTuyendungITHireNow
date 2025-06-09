f95d46432cfcdceff397ffacfe06a4240c9fee27
SonwTungf <sonwtungf.2004@gmail.com>
2025-06-09 21:47:53 +0700

    them seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ use Database\Seeders\CompanyReviewsSeeder;
 use Database\Seeders\SeekerProfilesSeeder;
 use Database\Seeders\EmployerPackagesSeeder;
 use Database\Seeders\ServicePackagesSeeder;
+// use Database\Seeders\job_applicationsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -53,6 +54,7 @@ class DatabaseSeeder extends Seeder
             ReportsSeeder::class,
             ApplicationsSeeder::class,
             ServicePackagesSeeder::class,
+            // job_applicationsSeeder::class,
         ]);
     }
 }
