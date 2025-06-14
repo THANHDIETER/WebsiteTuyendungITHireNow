1f3a25eeeb2c89a5441d4c267525a77fbdc3df0d
SonwTungf <sonwtungf.2004@gmail.com>
2025-06-14 21:09:10 +0700

    fix

--- note.php
+++ note.php
@@ -13,4 +13,15 @@ composer require league/oauth2-google -->
 <!-- 
  bootstrap
 npm install bootstrap @popperjs/core
+
+ntt-07/06/2025
+npm install jspdf html2canvas
+composer require barryvdh/laravel-dompdf
+
  -->
+ <!-- npm install
+chạy tailwindcs
+ npm install -D tailwindcss postcss autoprefixer
+
+
+php artisan migrate:fresh --seed -->
