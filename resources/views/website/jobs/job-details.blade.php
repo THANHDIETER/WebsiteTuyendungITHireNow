9b04ca148b93e4e114c1b7793abef00fb8908b7b
SonwTungf <sonwtungf.2004@gmail.com>
2025-06-16 21:08:23 +0700

    add detail

--- resources/views/website/jobs/job-details.blade.php
+++ resources/views/website/jobs/job-details.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <main class="main-content">
         <!--== Bắt đầu header trang ==-->
-        <div class="page-header-area sec-overlay sec-overlay-black" data-bg-img="../client/assets/img/banner/15.png">
+       <div class="page-header-area sec-overlay sec-overlay-black" data-bg-img="../client/assets/img/photos/bg2.webp">
             <div class="container pt--0 pb--0">
                 <div class="row">
                     <div class="col-12">
@@ -62,23 +62,21 @@
         </div>
 
         <!--== Kết thúc header trang ==-->
-        @if (session('success'))
-            <div class="alert alert-success alert-dismissible d-flex align-items-center p-3 rounded shadow-sm fade show"
-                role="alert">
-                <i class="bi bi-check-circle-fill me-2 fs-5"></i>
-                <div>{{ session('success') }}</div>
-                <button type="button" class="btn-close ms-auto" data-bs-dismiss="alert" aria-label="Đóng"></button>
-            </div>
-        @endif
+@if (session('success'))
+    <div class="alert alert-success alert-dismissible d-flex align-items-center p-3 rounded shadow-sm fade show" role="alert">
+        <i class="bi bi-check-circle-fill me-2 fs-5"></i>
+        <div>{{ session('success') }}</div>
+        <button type="button" class="btn-close ms-auto" data-bs-dismiss="alert" aria-label="Đóng"></button>
+    </div>
+@endif
 
-        @if (session('error'))
-            <div class="alert alert-danger alert-dismissible d-flex align-items-center p-3 rounded shadow-sm fade show"
-                role="alert">
-                <i class="bi bi-exclamation-triangle-fill me-2 fs-5"></i>
-                <div>{{ session('error') }}</div>
-                <button type="button" class="btn-close ms-auto" data-bs-dismiss="alert" aria-label="Đóng"></button>
-            </div>
-        @endif
+@if (session('error'))
+    <div class="alert alert-danger alert-dismissible d-flex align-items-center p-3 rounded shadow-sm fade show" role="alert">
+        <i class="bi bi-exclamation-triangle-fill me-2 fs-5"></i>
+        <div>{{ session('error') }}</div>
+        <button type="button" class="btn-close ms-auto" data-bs-dismiss="alert" aria-label="Đóng"></button>
+    </div>
+@endif
 
         <!--== Bắt đầu chi tiết công việc ==-->
         <section class="job-details-area">
