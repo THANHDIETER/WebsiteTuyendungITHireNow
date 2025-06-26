f3fe453f84f97253aac31d11f9dca91e335f4e35
sonbtph52402 <sonbtph52402@gmail.com>
2025-06-27 00:34:07 +0700

    len1

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Models\Category;
+use App\Models\Job;
 
 class BlogController extends Controller
 {
     public function index()
     {
-
         $blogs = Blog::latest()->paginate(6);
-        $categories = \App\Models\Category::all(); // ✅ thêm dòng này để lấy danh sách categories
-        $jobs = \App\Models\Job::latest()->paginate(6); // ✅ thêm dòng này để lấy danh sách jobs nếu cần
-        return view('website.index', compact('blogs', 'categories', 'jobs')); // ✅ sửa đúng: blogs, categories
+        $categories = Category::all();
+        $jobs = Job::latest()->paginate(6);
+
+        return view('website.index', compact('blogs', 'categories', 'jobs'));
     }
-
-
     public function indexGrid()
     {
         $blogs = Blog::latest()->paginate(6);
